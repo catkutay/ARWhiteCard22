6231e3a49c59d995e705e4c520a912cd1ba83510
Cat Kutay <cat.kutay@cdu.edu.au>
2022-05-22 13:13:25 +0930

    addin house scene

--- Assets/Scripts/mouseController.cs
+++ Assets/Scripts/mouseController.cs
@@ -1,75 +1,51 @@
-using UnityEngine;
 using System.Collections;
- 
-public class FlyCamera : MonoBehaviour {
- 
-    /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
-    wasd : basic movement
-    shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
-     
-     
-    float mainSpeed = 100.0f; //regular speed
-    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    float camSens = 0.25f; //How sensitive it with mouse
-    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
-    private float totalRun= 1.0f;
-     
-    void Update () {
-        lastMouse = Input.mousePosition - lastMouse ;
-        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
-        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
-        transform.eulerAngles = lastMouse;
-        lastMouse =  Input.mousePosition;
-        //Mouse  camera angle done.  
-       
-        //Keyboard commands
-        float f = 0.0f;
-        Vector3 p = GetBaseInput();
-        if (p.sqrMagnitude > 0){ // only move while a direction key is pressed
-          if (Input.GetKey (KeyCode.LeftShift)){
-              totalRun += Time.deltaTime;
-              p  = p * totalRun * shiftAdd;
-              p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-              p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-              p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-          } else {
-              totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-              p = p * mainSpeed;
-          }
-         
-          p = p * Time.deltaTime;
-          Vector3 newPosition = transform.position;
-          if (Input.GetKey(KeyCode.Space)){ //If player wants to move on X and Z axis only
-              transform.Translate(p);
-              newPosition.x = transform.position.x;
-              newPosition.z = transform.position.z;
-              transform.position = newPosition;
-          } else {
-              transform.Translate(p);
-          }
-        }
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseController : MonoBehaviour
+{
+    [SerializeField]private float _speed = 7f;
+    [SerializeField]private float _mouseSensitivity = 50f;
+    [SerializeField]private float _minCameraview = -70f, _maxCameraview = 80f;
+    private CharacterController _charController;
+    private Camera _camera;
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        _camera = Camera.main;
+
+        if(_charController == null)
+        Debug.Log("No Character Controller Attached to Player");
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
-     
-    private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
-        Vector3 p_Velocity = new Vector3();
-        if (Input.GetKey (KeyCode.W)){
-            p_Velocity += new Vector3(0, 0 , 1);
-        }
-        if (Input.GetKey (KeyCode.S)){
-            p_Velocity += new Vector3(0, 0, -1);
-        }
-        if (Input.GetKey (KeyCode.A)){
-            p_Velocity += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKey (KeyCode.D)){
-            p_Velocity += new Vector3(1, 0, 0);
-        }
-        return p_Velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get WASD Input for Player
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        //move player based on WASD Input
+        Vector3 movement = transform.forward * vertical + transform.right * horizontal; //changed this line.
+        _charController.Move(movement * Time.deltaTime * _speed);
+
+
+        
+        //Get Mouse position Input
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity; //changed this line.
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity; //changed this line.
+          //Rotate the camera based on the Y input of the mouse
+          xRotation -= mouseY;
+          //clamp the camera rotation between 80 and -70 degrees
+          xRotation = Mathf.Clamp(xRotation, _minCameraview, _maxCameraview);
+
+          _camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+          //Rotate the player based on the X input of the mouse
+          transform.Rotate(Vector3.up * mouseX * 3);
+
     }
 }
